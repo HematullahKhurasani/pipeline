b3208037036564c77a9cc206375b7d078e445e0f
HematullahKhurasani <56314707+HematullahKhurasani@users.noreply.github.com>
2021-09-16 12:24:52 +0100

    initial commit

---
+++ pipeline.bat
@@ -0,0 +1,28 @@
+pipeline {
+    agent any
+    stages {
+        stage ('compile') {
+            steps {
+                echo "compiled Successfully !";
+            }
+        }
+    }
+
+    post {
+        always {
+            echo 'This will always run'
+        }
+        success {
+            echo 'This will run only if successful'
+        }
+        failure {
+            echo 'This will run only if failed'
+        }
+        unstable {
+            echo 'This will run only if the run was marked as unstable'
+        }
+        changed {
+            echo 'This will run only if the state of the Pipeline has changed'
+        }
+    }
+}
